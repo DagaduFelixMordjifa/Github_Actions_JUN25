c3de8c6e0f0949f71a5d5361d558ea78ad4801ac
DagaduFelixMordjifa <dagadufelix@gmail.com>
2025-06-10 14:11:49 +0000

    style: Run terraform fmt to fix formatting

--- TFvars/tvars.tf
+++ TFvars/tvars.tf
@@ -1,3 +1,3 @@
-aws_region     = "us-west-2"
-ami_id         = "ami-0a605bc2ef5707a18"
-instance_type  = "t2.micro"
+aws_region    = "us-west-2"
+ami_id        = "ami-0a605bc2ef5707a18"
+instance_type = "t2.micro"
